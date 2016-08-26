86c75ce87341603021853ef03a23e1f5c522119a
Ehsan Golesorkhi <ehsan_golesorkhi@epam.com>
2016-08-26 12:10:22 +0200

    basic configurations for spring is done

---
+++ WebClient/src/main/java/service/home/HelloWorld.java
@@ -0,0 +1,25 @@
+package service.home;
+
+import model.BookData;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ehsan on 26.08.16.
+ */
+@RestController
+public class HelloWorld {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/home")
+    public BookData getData(){
+        return BookData.builder().tag("test").
+                title("test").
+                url("test").
+                library("test").
+                author("test").
+                description("test").
+                price("0").build();
+    }
+
+}
