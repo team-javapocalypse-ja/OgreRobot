3266ffbc0621271852a0b2bd2c329c63df10b389
Tomasz Dudzik <tomaszdudzik.web@gmail.com>
2016-08-29 11:18:48 +0200

    Add a possibility of extracting books only with a certain category/categories.

--- RobotCrawler/src/main/java/parser/empik/EmpikParser.java
+++ RobotCrawler/src/main/java/parser/empik/EmpikParser.java
@@ -2,7 +2,11 @@ package parser.empik;
 
 import model.BookData;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class EmpikParser {
 
@@ -20,9 +24,35 @@ public class EmpikParser {
     }
 
     public List<BookData> parse() {
+        return parse(new ArrayList<>());
+    }
+
+    public List<BookData> parse(List<String> categories) {
         List<String> categoryPageUrls = promotionsPageParser.extractCategoryPageUrls();
         List<String> bookPageUrls = categoryPageParser.extractBookPageUrls(categoryPageUrls);
-        return bookPageParser.extractBooks(bookPageUrls);
+        List<BookData> booksData = bookPageParser.extractBooks(bookPageUrls);
+
+        Predicate<BookData> categoryPredicate = new CategoryPredicate(categories);
+        return booksData.stream().filter(categoryPredicate).collect(Collectors.toList());
+    }
+
+    private static class CategoryPredicate implements Predicate<BookData> {
+
+        private final List<String> categories;
+
+        public CategoryPredicate(List<String> categories) {
+            this.categories = categories;
+        }
+
+        @Override
+        public boolean test(BookData bookData) {
+            if (categories.isEmpty()) {
+                return true;
+            }
+
+            return categories.contains(bookData.tag);
+        }
+
     }
 
 }
