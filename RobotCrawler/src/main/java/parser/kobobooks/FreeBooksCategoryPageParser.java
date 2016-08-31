3831b742dad30ede36cd6b8571cc5657e0781a52
lukasz_czubernat <lukasz_czubernat@epam.com>
2016-08-31 09:42:51 +0200

    KoboBooks parser I've create prototype parser for kobobooks.com. Parser get almost all data about books that are required (except tags) This implementation need few refactors to be done and more optimalization, bacause parsing from that site have high time complexity.

---
+++ RobotCrawler/src/main/java/parser/kobobooks/FreeBooksCategoryPageParser.java
@@ -0,0 +1,30 @@
+package parser.kobobooks;
+
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FreeBooksCategoryPageParser {
+
+    private KoboPageDownloader koboPageDownloader;
+
+    public FreeBooksCategoryPageParser(KoboPageDownloader koboPageDownloader) {
+        this.koboPageDownloader = koboPageDownloader;
+    }
+
+    public List<String> collectBooksUrls(String categoryUrl) {
+        Document doc = koboPageDownloader.getDocument(categoryUrl).get();
+        return collectAllBooksUrls(doc.getElementsByClass("item-info"));
+    }
+
+    private List<String> collectAllBooksUrls(Elements categoryElements) {
+        return categoryElements.parallelStream().map(element -> element.getElementsByClass("title")
+                .first()
+                .select("a")
+                .attr("href")).collect(Collectors.toList());
+    }
+}
+
