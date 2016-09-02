50cabb7317bb30fd145307e4eecd7d137406f38b
lukasz_czubernat <lukasz_czubernat@epam.com>
2016-09-02 10:58:50 +0200

    Refactor of logic I've fixed and extended logic of parser for kobobooks.com page. I still need to get rid of msvm in KoboBooksParser but I'll do this as soon as I rebese master on this branch. Then I'll add some tests for it. I know I broke TDD manner but in this case developing in TDD manner will be to hard and time consuming. Most of a time I spend on debugging and finding out how can I get date from html.

--- RobotCrawler/src/main/java/parser/kobobooks/KoboBooksParser.java
+++ RobotCrawler/src/main/java/parser/kobobooks/KoboBooksParser.java
@@ -6,31 +6,16 @@ public class KoboBooksParser {
 
     public static final String freeBooksUrl = "/p/free-ebooks";
 
-    private KoboPageDownloader koboPageDownloader;
-
-    public KoboBooksParser(KoboPageDownloader koboPageDownloader) {
-        this.koboPageDownloader = koboPageDownloader;
-    }
-
-    private static String parseCategoryNameFromUrl(String url) {
-        String[] parts = url.split("/");
-        return parts[parts.length - 2];
-    }
-
     public static void main(String[] args) {
         KoboPageDownloader koboPageDownloader = new KoboPageDownloader();
-        FreeBooksPageParser pageParser = new FreeBooksPageParser(koboPageDownloader);
-        FreeBooksCategoryPageParser categoryPageParser = new FreeBooksCategoryPageParser(koboPageDownloader);
-        CategoryBooksParser booksParser = new CategoryBooksParser(koboPageDownloader, new KoboBookDataCollector());
-
-        KoboBooksParser parser = new KoboBooksParser(koboPageDownloader);
-
+        KoboBooksPageParser pageParser = new KoboBooksPageParser(koboPageDownloader);
+        KoboBooksCategoryPageParser categoryPageParser = new KoboBooksCategoryPageParser(koboPageDownloader);
+        KoboBooksCategoryParser booksParser = new KoboBooksCategoryParser(koboPageDownloader, new KoboBookDataCollector());
 
         List<String> categoriesUrls = pageParser.getFreeBooksCategoriesUrls(freeBooksUrl);
         List<String> booksUrls = categoryPageParser.collectBooksUrls(categoriesUrls.get(0));
-        ;
 
-        System.out.println(booksParser.collectBookData(booksUrls));
+        booksParser.collectBookData(booksUrls).stream().forEach(System.out::println);
     }
 
     private void parse() {
