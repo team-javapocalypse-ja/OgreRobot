07355da919a5be9f2347dc43ea012296ca754225
Tomasz Dudzik <tomaszdudzik.web@gmail.com>
2016-09-07 14:04:34 +0200

    Create mappers from DTO to entity and from entity to DTO.

---
+++ Core/src/main/java/javapocalypse/core/api/config/ModelMapperConfiguration.java
@@ -0,0 +1,19 @@
+package javapocalypse.core.api.config;
+
+import javapocalypse.core.api.mappers.converters.LibraryConverter;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfiguration {
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.addConverter(new LibraryConverter());
+
+        return modelMapper;
+    }
+
+}
