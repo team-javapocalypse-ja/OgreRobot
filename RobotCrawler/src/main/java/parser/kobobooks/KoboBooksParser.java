18f16165b099aa1120daf62267dc298878f1e2cf
lukasz_czubernat <lukasz_czubernat@epam.com>
2016-09-07 12:12:51 +0200

    Refactor after code review I've refactored all classes and connected them to common 'downloader'. It's first step into ingegration with rest of parsers. Also I've added looging to all classes - 'log them all'

--- RobotCrawler/src/main/java/parser/kobobooks/KoboBooksParser.java
+++ RobotCrawler/src/main/java/parser/kobobooks/KoboBooksParser.java
@@ -1,32 +1,42 @@
 package parser.kobobooks;
 
+import lombok.extern.log4j.Log4j2;
 import model.BookData;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
+@Log4j2
 public class KoboBooksParser {
 
+    private static final String baseUrl = "https://store.kobobooks.com";
     private static final String freeBooksUrl = "/p/free-ebooks";
-    KoboBooksPageParser booksPageParser;
-    KoboBooksCategoryPageParser booksCategoryPageParser;
-    KoboBooksCategoryParser booksCategoryParser;
-    private KoboPageDownloader pageDownloader;
+    PromoPageParser booksPageParser;
+    CategoryPageParser booksCategoryPageParser;
+    CategoryParser booksCategoryParser;
 
-    public KoboBooksParser(KoboPageDownloader pageDownloader) {
-        this.pageDownloader = pageDownloader;
-        this.booksPageParser = new KoboBooksPageParser(pageDownloader);
-        this.booksCategoryPageParser = new KoboBooksCategoryPageParser(pageDownloader);
-        this.booksCategoryParser = new KoboBooksCategoryParser(pageDownloader, new KoboBookDataCollector());
+    public KoboBooksParser() {
+        this.booksPageParser = new PromoPageParser();
+        this.booksCategoryPageParser = new CategoryPageParser();
+        this.booksCategoryParser = new CategoryParser(new KoboBooksDataCollector());
     }
 
     public Map<String, List<BookData>> parse() {
-        List<String> categoriesUrls = booksPageParser.getFreeBooksCategoriesUrls(freeBooksUrl);
-        List<String> booksUrls = booksCategoryPageParser.collectBooksUrls(categoriesUrls.get(0));
+        Map<String, List<BookData>> books = new TreeMap<>();
+        try {
+            List<String> categoriesUrls = booksPageParser.getPromoCategoriesUrls(baseUrl.concat(freeBooksUrl));
+            List<String> booksUrls = booksCategoryPageParser.collectBooksUrls(categoriesUrls.get(0));
 
-        return booksCategoryParser.collectBookData(booksUrls)
-                .parallelStream()
-                .collect(Collectors.groupingBy(b -> b.tag));
+            books = booksCategoryParser.collectBookData(booksUrls)
+                    .parallelStream()
+                    .collect(Collectors.groupingBy(book -> book.tag));
+        } catch (IOException e) {
+            log.error("Error during parsing ".concat(e.getMessage()));
+        }
+        return books;
     }
 }
