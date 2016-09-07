acebc65dd9f4fb3629afedaa5aa11aed511f0868
Ehsan Golesorkhi <ehsan_golesorkhi@epam.com>
2016-09-07 10:42:04 +0200

    fixed from code review

--- RobotCrawler/src/main/java/robot/ebooks/EBooksRobotManager.java
+++ RobotCrawler/src/main/java/robot/ebooks/EBooksRobotManager.java
@@ -33,7 +33,7 @@ public class EBooksRobotManager implements Robot {
         }
     }
 
-    public boolean hasToFind(EBookCategory category) {
+    public boolean hasToFind(BookTag category) {
         return tasks.containsKey(category);
     }
 
