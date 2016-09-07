7888fb21a6f738f88ed0538e43c770ba85bd90ec
lukasz_czubernat <lukasz_czubernat@epam.com>
2016-09-07 16:28:44 +0200

    Fixes after code review I've changed few things after code review

--- RobotCrawler/src/main/java/parser/empik/PageDownloader.java
+++ RobotCrawler/src/main/java/parser/empik/PageDownloader.java
@@ -1,11 +1,13 @@
 package parser.empik;
 
+import lombok.extern.log4j.Log4j2;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.util.Optional;
 
+@Log4j2
 public class PageDownloader {
 
     public Optional<Document> downloadPage(String url) {
@@ -13,6 +15,7 @@ public class PageDownloader {
         try {
             document = Jsoup.connect(url).get();
         } catch (IOException e) {
+            log.error("Error during establishing connection to ".concat(url));
             document = null;
         }
 
